7a8e49ce35758cd04a1108a185e7f757f6814c46
Roshenchik <asalex2001@mail.ru>
2023-01-17 22:37:31 +0300

    feat: add menu icon

--- plants/index.js
+++ plants/index.js
@@ -1,1 +1,10 @@
 console.log('all tasks have been implemented\nsmall deviations from template, but not more 10px\nscore: 100')
+
+/*                                               */
+
+let headerMenu = document.getElementsByClassName('header__menu')[0];
+let headerList = document.getElementsByClassName('header__list')[0];
+
+headerMenu.addEventListener('click', () => {
+	headerList.classList.toggle('active');
+})
